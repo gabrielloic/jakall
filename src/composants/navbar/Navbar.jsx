a019588427a16296d66f30ce2643ce8e812dd933
gabrielloic <loicnlep@gmail.com>
2025-10-13 15:27:55 +0100

    Premier commit

---
+++ src/composants/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <img src={assets.} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
